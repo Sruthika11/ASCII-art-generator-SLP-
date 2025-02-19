1868dd6852e4e0b3ecde98085e4fe933a76716bc
Sruthika11 <sruthika1103@gmail.com>
2025-02-19 19:39:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 function generateArt() {
     let text = document.getElementById("textInput").value;
     
-    // Issue: ASCII generation function is incorrect
+    // Check if there's any text input
+    if (text.trim() === "") {
+        document.getElementById("asciiOutput").innerText = "Please enter some text!";
+        return;
+    }
+
+    // Generate ASCII codes of the input text
     let asciiArt = text.split("").map(char => char.charCodeAt(0)).join(" ");
     
     document.getElementById("asciiOutput").innerText = asciiArt;
 }
 
 function clearArt() {
-
     document.getElementById("asciiOutput").innerText = "";
-    document.getElementById("textInput").value="";
+    document.getElementById("textInput").value = "";
 }
